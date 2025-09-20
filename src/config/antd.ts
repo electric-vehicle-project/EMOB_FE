50e3313f9d0d1f82cdef6573014240d94049da97
TruongV2905 <truong0345218921@gmail.com>
2025-09-20 13:41:08 +0700

    chore(antd) : config button

--- src/config/antd.ts
+++ src/config/antd.ts
@@ -2,7 +2,7 @@ import type { ThemeConfig } from "antd";
 
 export const theme: ThemeConfig = {
   components: {
-    Checkbox: {
+    Button: {
       colorPrimary: "#000000",
       colorPrimaryHover: "#000000",
       colorBorder: "#000000",
