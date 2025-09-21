c58fc1c79e611da90600f6f0425be833d33044a8
tnhdarkrai1457@gmail.com <tnhdarkrai1457@gmail.com>
2025-09-21 16:26:36 +0700

    style(button): adjust button

--- src/config/antd.ts
+++ src/config/antd.ts
@@ -1,11 +1,18 @@
 import type { ThemeConfig } from "antd";
 
 export const theme: ThemeConfig = {
+<<<<<<< Updated upstream
   components: {
     Button: {
       colorPrimary: "#000000",
       colorPrimaryHover: "#000000",
       colorBorder: "#000000",
     },
+=======
+  token: {
+    colorPrimary: "#627254",
+    colorPrimaryHover: "#515f45ff",
+    colorBorder: "#627254",
+>>>>>>> Stashed changes
   },
 };
