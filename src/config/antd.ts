6cef033ddf8e8ee11890b9027963517c5a4b314d
TruongV2905 <truong0345218921@gmail.com>
2025-09-24 16:03:25 +0700

    fix: update button theme colors to match design specifications

--- src/config/antd.ts
+++ src/config/antd.ts
@@ -3,9 +3,17 @@ import type { ThemeConfig } from "antd";
 export const theme: ThemeConfig = {
   components: {
     Button: {
-      colorPrimary: "#000000",
-      colorPrimaryHover: "#000000",
-      colorBorder: "#000000",
+      // Màu primary
+      colorPrimary: "#627254",
+      colorPrimaryHover: "#525e46",
+      colorPrimaryActive: "#414d38",
+      colorPrimaryBg: "#627254",
+      controlTmpOutline: "rgba(98, 114, 84, 1)",
+
+      //Màu default
+      colorBgSolid: "#24282B",
+      defaultHoverBg: "#262626",
+      defaultHoverColor: "#D9D9D9",
     },
   },
 };
