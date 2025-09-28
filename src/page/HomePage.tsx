d1fd0b6b3922cd36f0950ddda7553fac755f0810
TruongV2905 <truong0345218921@gmail.com>
2025-09-29 03:04:19 +0700

    fix(: adjust layout for overflow and align EMOB text

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -4,29 +4,37 @@ import { Button } from "antd";
 const HomePage: React.FC = () => {
   return (
     <div
-      className="relative w-full h-screen bg-cover bg-center"
+      className="relative w-full h-screen bg-cover bg-center overflow-hidden"
       style={{ backgroundImage: "url('/bg_Green.png')" }}
     >
-      {/* Logo */}
-      <img
-        src="/logo.png"
-        alt="EMOB Logo"
-        className="absolute top-8 left-11 w-[95px] h-[95px] rounded-full z-20"
-      />
+      <div className="w-full px-10  py-6 flex justify-between  items-center">
+        {/* Logo */}
+        <img
+          src="/logo.png"
+          alt="EMOB Logo"
+          className="w-[5vw] aspect-square rounded-full z-20"
+        />
 
-      {/* Button */}
-      <Button
-        type="default"
-        className="!absolute !top-12 !right-12 !px-8 !py-4 !text-lg !font-bold !rounded-xl !h-auto !w-auto z-20"
-      >
-        ĐĂNG NHẬP
-      </Button>
-
+        {/* Button */}
+        <Button
+          type="default"
+          size="large" 
+          style={{
+            width: "200px",
+            height: "60px",
+            fontSize: "18px",
+            fontWeight: "700",
+            borderRadius: "12px",
+          }}
+        >
+          ĐĂNG NHẬP
+        </Button>
+      </div>
       {/* chữ EMOB */}
       <h1
-        className="absolute inset-0 flex items-center justify-center 
+        className="absolute inset-0 flex items-end justify-center 
            font-teko font-bold leading-none select-none 
-           z-10 scale-y-100"
+           z-10"
         style={{
           fontFamily: "'Teko', sans-serif",
           fontWeight: "530",
