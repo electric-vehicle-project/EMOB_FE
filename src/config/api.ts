de3710aeb1e19d54363ec13b1a7b4ad287491520
FarleyNGX <minhthanh2012005@gmail.com>
2025-10-20 17:23:56 +0700

    fix(api): revert baseURL to localhost for local development

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://103.200.20.149/:8080/api/",
-  baseURL: "http://34.56.57.56:8080/api",
+  baseURL: "http://localhost:8080/api",
 });
 
 api.interceptors.request.use(
