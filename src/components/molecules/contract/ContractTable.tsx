67796ea29ea336da1bcfd0561c71721cdc66ba61
FarleyNGX <minhthanh2012005@gmail.com>
2025-11-15 22:29:17 +0700

    feat(contract): add ellipsis dropdown to table
    - Added ellipsis icon
    - Implemented dropdown menu
    - Menu options defined

--- src/components/molecules/contract/ContractTable.tsx
+++ src/components/molecules/contract/ContractTable.tsx
@@ -141,6 +141,8 @@ export const ContractTable = ({ data }: Props) => {
           />
         );
 
+
+        
         return (
           <Dropdown overlay={menu} trigger={["click"]} placement="bottomRight">
             <EllipsisOutlined className="text-2xl cursor-pointer text-gray-600 hover:text-black" />
